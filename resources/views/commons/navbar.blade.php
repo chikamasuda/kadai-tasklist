1fbe4fac46a3fc90e2295d1fa40e44292bf79d7e
chika.masuda <c.m.062820@gmail.com>
2020-06-28 10:15:27 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,13 +8,17 @@
         
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
+            <ul class="nav navbar-nav">
+                @if (Auth::check())
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('logout.get', 'Logout', [],['class' =>'nav-link']) !!}</li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+                @else
                 <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' =>'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
 </header>
+
